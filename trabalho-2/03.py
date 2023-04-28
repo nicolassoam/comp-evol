4d6e56d54a474ffa7dc7be7355d29f362ff15032
nicolassoam <nsm4456@gmail.com>
2023-04-28 20:30:47 -0300

    ignoring the usage of all iterations

--- trabalho-2/03.py
+++ trabalho-2/03.py
@@ -7,7 +7,7 @@ import mealpy.swarm_based.ALO as alo
 alg = 'f3'
 dim = 10
 executions = 1000
-files = []
+results = []
 
 def fit(solution):
         x = np.random.uniform(-100, 100, size=(executions, dim))
@@ -22,7 +22,7 @@ term_dict = {
     'max_fe': 10000 * dim,
 }
 
-# ga1 = ga.EliteMultiGA(51,1000,0.85,0.01)
+# ga1 = ga.EliteMultiGA(1000,1000,0.85,0.01)
 ga1 = de.SHADE(1000,1000)
 # ga1 = alo.BaseALO(1000,1000)
 using_alg = ga1.name
@@ -38,24 +38,20 @@ for i in range(0,3):
         "n_dims":dim,
         # 'obj_weights':np.ones(3)
         "obj_weights":np.ones(executions),
-        "log_to":"file",
-        "log_file":f"results/result_{i}_{using_alg}_{dim}_{alg}.txt"
     }
     
     
     # file = open(f"results/result_{i}_{using_alg}_{dim}_{alg}.txt", "w")
     
     # file.write(f"Execucao {i}: \n")
-    ga1.solve(problem, 'thread', termination = term_dict)
+    best_fit, best_sol = ga1.solve(problem, 'thread', termination = term_dict)
     
-    files.append(f"results/result_{i}_{using_alg}_{dim}_{alg}.txt")
+    results.append(best_sol)
     
     # file.close()
     
-with open(f'results/result_{using_alg}_{dim}_{alg}','w') as compiled:
-    for fname in files:
-        with open(fname) as infile:
-            for line in infile:
-                compiled.write(line)
+with open(f'results/result_{using_alg}_{dim}_{alg}.txt','w') as compiled:
+    for sol in results:
+        compiled.write(f'{sol}\n')
 
     
