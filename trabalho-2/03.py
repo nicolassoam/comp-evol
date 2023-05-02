1434b5f271a8d29656c778b480a0ca9e16b259d4
nicolassoam <nsm4456@gmail.com>
2023-05-02 02:17:58 -0300

    trying different metaheuristics

--- trabalho-2/03.py
+++ trabalho-2/03.py
@@ -1,16 +1,43 @@
-from cec2017.functions import f4 as func
+from cec2017.functions import f6 as func
 import numpy as np
 import mealpy.evolutionary_based.GA as ga
 import mealpy.evolutionary_based.DE as de
-import mealpy.swarm_based.ALO as alo
+import mealpy.swarm_based.BeesA as ba
 import os 
 
+# from ctypes import CDLL, POINTER, c_int, c_double
+# import pathlib
+
+# def cec17_test_func(x, f, nx, mx, func_num,
+#                     ):
+#     libname = f"{pathlib.Path().absolute()}\cec17_test_func.so"
+#     print(libname)
+#     dll_path=CDLL(libname)
+#     functions = dll_path
+#     x_pointer_type = POINTER(c_double * nx)
+#     f_pointer_type = POINTER(c_double * mx)
+#     nx_type = c_int
+#     mx_type = c_int
+#     func_num_type = c_int
+#     functions.cec17_test_func.argtypes = [x_pointer_type, f_pointer_type,
+#                                           nx_type, mx_type, func_num_type] 
+#     functions.cec17_test_func.restype = None
+#     x_ctype = (c_double * nx)()
+#     for i, value in enumerate(x):
+#         x_ctype[i] = value
+#     f_ctype = (c_double * mx)()
+#     for i in range(mx):
+#         f_ctype[i] = 0
+#     functions.cec17_test_func(x_pointer_type(x_ctype), f_pointer_type(f_ctype),
+#                               nx, mx, func_num)
+#     for i in range(len(f)):
+#         f[i] = f_ctype[i]
 
 path = 'results'
 if not os.path.exists(path):
     os.makedirs(path)
 
-n_times = 52
+n_times = 51
 alg = func.__name__
 
 dim = int(input("Dimensão: "))
@@ -19,19 +46,23 @@ executions = 1000
 results = []
 
 def fit(solution):
-        x = np.random.uniform(-100, 100, size=(executions, dim))
-        val = func(x);
+        # x = np.random.uniform(-100, 100, size=(executions, dim))
+        solution = np.reshape(solution, (1, dim))
+        # print(solution)
+        # resp = POINTER(c_double * dim)
+        # val =cec17_test_func(solution, resp, dim, executions, 2);
+        val  = func(solution)
 
-        return val.min()
+        return val
 
     
 term_dict = {
     'max_fe': 10000 * dim,
 }
 
-# ga1 = ga.EliteMultiGA(1000,1000,0.85,0.01)
-ga1 = de.SHADE(1000,1000)
-# ga1 = alo.BaseALO(1000,1000)
+# ga1 = ga.EliteSingleGA(3500,50,pc = 0.85,pm =0.025,selection='tournament')
+# ga1 = de.SHADE(epoch = 3500,pop_size = 50, miu_f=0.5, miu_cr=0.5)
+ga1 = ba.CleverBookBeesA(3500,50)
 using_alg = ga1.name
 
 
@@ -48,7 +79,7 @@ for i in range(0,n_times):
     
     # file = open(f"results/result_{i}_{using_alg}_{dim}_{alg}.txt", "w")
     
-    best_fit, best_sol = ga1.solve(problem, 'thread', termination = term_dict)
+    best_fit, best_sol = ga1.solve(problem,'thread', termination = term_dict,n_workers=6)
     
     results.append(best_sol)
     
