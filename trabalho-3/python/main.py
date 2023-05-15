54696f97b6f57d56de844a0de85665afd70f8eff
nicolassoam <nsm4456@gmail.com>
2023-05-15 19:31:10 -0300

    implementacao com mealpy

---
+++ trabalho-3/python/main.py
@@ -0,0 +1,42 @@
+import tsplib95
+import numpy as np
+import mealpy.evolutionary_based.GA as ga
+import mealpy.evolutionary_based.DE as de
+import mealpy.swarm_based.PSO as pso
+import os 
+from tsp import TSPProblem as TSP, returnMatrix
+
+path = 'results'
+if not os.path.exists(path):
+    os.makedirs(path)
+
+RESULTS = []
+
+path_atsp = 'instances/atsp'
+path_tsp = 'instances/tsp'
+
+problem = tsplib95.load(f'{path_atsp}/br17.atsp')
+distance = returnMatrix(problem)
+
+lb = [0, ] * len(distance)
+ub = [len(distance)-1, ] * len(distance)
+
+term_dict = {
+    'max_fe': 20000,
+}
+
+algo = pso.C_PSO(3500,40)
+
+using_algo = algo.name
+t = TSP(lb = lb,ub = ub,minmax='min',distance_matrix= distance, n_dims = problem.dimension, obj_weights = np.ones(problem.dimension))
+
+best_fit, best_sol = algo.solve(t,'thread', termination = term_dict,n_workers=6)
+
+RESULTS.append(best_sol)
+
+with open(f'results/result_{using_algo}_{problem.dimension}_{problem.name}.txt','w') as compiled:
+    i = 0
+    for sol in RESULTS:
+        compiled.write(f'Execução {i}: {sol}\n')
+        i += 1
+
--- trabalho-3/python/tsp.py
+++ trabalho-3/python/tsp.py
@@ -1,42 +1,36 @@
 import numpy as np
-import mealpy.evolutionary_based.GA as ga
-import mealpy.evolutionary_based.DE as de
-import mealpy.swarm_based.BeesA as ba
-import os
-from mealpy import Problem 
-import tsplib95
 import networkx
+from mealpy import Problem 
 
-
-path_atsp = 'instances/atsp'
-path_tsp = 'instances/tsp'
-
-problem = tsplib95.load(f'{path_atsp}/br17.atsp')
-
-g = problem.get_graph()
-
-distance = networkx.to_numpy_matrix(g)
-
-
-class TSP(Problem):
-    def __init__(self, lb, ub,minmax,distance_matrix = None,**kwargs):
-        super().__init__(lb, ub, minmax,**kwargs)
+class TSPProblem(Problem):
+    def __init__(self, lb, ub,minmax,name="TSP",distance_matrix = None,**kwargs):
+        self.name = name
         self.distance_matrix = distance_matrix
+        super().__init__(lb, ub, minmax,**kwargs)
         
-    def generate_position(self, lb=None, ub=None):
-        return np.random.permutation(len(self.distance_matrix))
-    
-    def amend_position(self, solution, lb=None, ub=None):
-        np.clip(solution, lb,ub)
+        def generate_position(lb=None, ub=None):
+            return np.random.permutation(len(lb))
         
+        def amend_position(solution,lb=None, ub=None):
+            solution = np.clip(solution, lb,ub)
+            
+            solution_int = solution.astype(int)
+            return solution_int
         
-    
     def fit_func(self, solution):
-        dist = self.distance_matrix[solution]
+        solution = np.clip(solution, self.lb,self.ub)
+        dist = self.distance_matrix[solution.astype(int)]
         sum =0.
         for i in range(len(dist)-1):
             sum += dist[i,i+1]
         sum += dist[-1,0]
         return sum
+
+
+def returnMatrix(problem):
+    g = problem.get_graph()
+    distance = networkx.to_numpy_matrix(g)
+    return distance
+
             
     
