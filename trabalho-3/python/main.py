a3a5b60bf76c483c51cd9b749e3d0f6d17a0b94d
nicolassoam <nsm4456@gmail.com>
2023-05-30 01:51:39 -0300

    resultados

--- trabalho-3/python/main.py
+++ trabalho-3/python/main.py
@@ -4,7 +4,8 @@ import mealpy.evolutionary_based.GA as ga
 import mealpy.evolutionary_based.DE as de
 import mealpy.swarm_based.PSO as pso
 import os 
-from tsp import TSPProblem as TSP, returnMatrix, returnProblem
+import mealpy.utils.problem as Problem
+from tsp import TSPProblem ,returnMatrix, returnProblem
 
 path = 'results'
 if not os.path.exists(path):
@@ -20,65 +21,28 @@ FTV70 = 'instances/atsp/ftv70.atsp'
 
 problem_tsp = returnProblem(FTV70,'atsp')
 distance = returnMatrix(problem_tsp)
+# distance = np.triu(distance)
+lb = [0, ] * problem_tsp.dimension
+ub = [problem_tsp.dimension-1, ] * problem_tsp.dimension
 
-
-def fit_func(solution):
-    solution = np.random.permutation(problem_tsp.dimension)
-    solution = np.clip(solution, [0,]*problem_tsp.dimension,[problem_tsp.dimension-1,]*problem_tsp.dimension)
-    # print(solution)
-    # print(distance[solution.astype(np.int64)])
-    # dist = distance[solution.astype(np.int64)]
-    # print(dist)
-    solution = solution.astype(np.int64)
-    sum =0.
-    for i in range(len(distance)-1):
-        k,j = solution[i],solution[i+1]	
-        sum += distance[k,j]
-
-    k,j = solution[-1],solution[0]	
-
-    sum += distance[k,j]
-    return sum
-
-def generate_position(lb=None, ub=None):
-    return np.random.permutation(len(lb))
-    
-def amend_position(solution,lb=None, ub=None):
-    solution = np.random.permutation(len(lb))
-    solution = np.clip(solution, lb,ub)
-    
-    solution_int = solution.astype(np.uint64)
-    
-    return solution_int
-
-# lb = [0, ] * len(distance)
-# ub = [len(distance)-1, ] * len(distance)
-
-problem = {
-    "fit_func": fit_func,
-    "lb": [0, ] * problem_tsp.dimension,
-    "ub": [problem_tsp.dimension-1, ] * problem_tsp.dimension,
-    "minmax":"min",
-    "n_dims":problem_tsp.dimension,
-    "obj_weights":np.ones(len(distance)),
-    "generate_position": generate_position,
-    "amend_position": amend_position,
-}
 
 term_dict = {
     'max_fe': 70000,
 }
 
-algo = pso.C_PSO(4500,20)
+# algo = pso.C_PSO(4500,10)
+# algo = ga.EliteSingleGA(10000,10)
+algo = de.BaseDE(10000,10)
 
 using_algo = algo.name
-# t = TSP(lb = lb,ub = ub,minmax='min',distance_matrix= distance, n_dims = problem.dimension, obj_weights = np.ones(problem.dimension))
 
-best_fit, best_sol = algo.solve(problem,'thread', termination = term_dict,n_workers=6)
+t = TSPProblem(lb = lb,ub = ub,minmax='min',distance_matrix= distance, n_dims = problem_tsp.dimension, obj_weights = np.ones(problem_tsp.dimension))
+
+best_fit, best_sol = algo.solve(t,'thread', termination = term_dict,n_workers=6)
 
 RESULTS.append(best_sol)
 
-with open(f'results/result_{using_algo}_{problem_tsp.dimension}_{problem_tsp.name}.txt','w') as compiled:
+with open(f'results/result_{using_algo}_{problem_tsp.dimension}_{problem_tsp.name}.txt','a') as compiled:
     i = 0
     for sol in RESULTS:
         compiled.write(f'Execução {i}: {sol}\n')
--- trabalho-3/python/tsp.py
+++ trabalho-3/python/tsp.py
@@ -3,25 +3,40 @@ import networkx
 from mealpy.utils.problem import Problem 
 import tsplib95
 class TSPProblem(Problem):
-    def __init__(self, lb, ub,minmax,name="TSP",distance_matrix = None,**kwargs):
-        
-        self.name = name
+    def __init__(self, lb, ub,minmax, distance_matrix,name="TSPProblem",**kwargs):
         self.distance_matrix = distance_matrix
-       
-        def generate_position(lb=None, ub=None):
-            return np.random.permutation(len(lb))
-        
-        def amend_position(solution,lb=None, ub=None):
-            solution = np.clip(solution, lb,ub)
-            solution_int = solution.astype(np.int64)
-            return solution_int
         super().__init__(lb, ub, minmax,**kwargs)
+        self.name = name
+        
+    def generate_position(self,lb=None, ub=None):
+        return np.random.permutation(len(lb))
+    
+    def amend_position(self,solution,lb=None, ub=None):
+        solution = np.clip(solution, lb,ub)
+        # solution_int = solution.astype(np.int64)
+        solution_set = set(list(range(0, len(solution))))
+        solution_done = np.array([-1, ] * len(solution))
+        solution_int = solution.astype(np.int32)
+        city_unique, city_counts = np.unique(solution_int, return_counts=True)
+
+        for idx, city in enumerate(solution_int):
+            if solution_done[idx] != -1:
+                continue
+            if city in city_unique:
+                solution_done[idx] = city
+                city_unique = np.where(city_unique == city, -1, city_unique)
+            else:
+                list_cities_left = list(solution_set - set(city_unique) - set(solution_done))
+                solution_done[idx] = list_cities_left[0]
+        # print(f"Final solution: {solution_done}")
+        # solution_done = np.clip(solution_done, lb,ub)
+        return solution_done
+        # return solution_int
         
         
     def fit_func(self, solution):
-        solution = np.random.permutation(len(self.lb))
         solution = np.clip(solution, self.lb,self.ub)
-        dist = self.distance_matrix[solution.astype(np.int64)]
+        dist = self.distance_matrix[solution]
         sum =0.
         for i in range(len(dist)-1):
             sum += dist[i,i+1]
