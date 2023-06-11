f8ffed9f354cba2ac9b1e56b494e82c999362665
nicolassoam <nsm4456@gmail.com>
2023-06-11 12:04:50 -0300

    deixando mais legível

--- trabalho-4/include/config.h
+++ trabalho-4/include/config.h
@@ -1,2 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #define PROJECT_DIR "C:/Users/Nicolas/Desktop/comp-evol/trabalho-4/"
-#define INSTANCES "C:/Users/Nicolas/Desktop/comp-evol/trabalho-4/instances" 
+#define INSTANCES "C:/Users/Nicolas/Desktop/comp-evol/trabalho-4/instances"
+#define FALKENAUER_U "C:/Users/Nicolas/Desktop/comp-evol/trabalho-4/instances/Falkenauer/Falkenauer_U"
+#define FALKENAUER_T "C:/Users/Nicolas/Desktop/comp-evol/trabalho-4/instances/Falkenauer/Falkenauer_T" 
+
+
+#endif
--- trabalho-4/src/main.cpp
+++ trabalho-4/src/main.cpp
@@ -9,7 +9,7 @@ int main(){
     srand(SEED);
     int problem_dimension = 10;
     int population_size = 1000 * problem_dimension;
-    int epochs = 1000;
+    int epochs = 2000;
     double wf = 0.8;
     double cr = 0.9;
     double_vec lb(problem_dimension, 0);
@@ -18,11 +18,11 @@ int main(){
     DiffEvol de(population_size, problem_dimension, epochs, lb, ub, wf, cr);
 
     std::ifstream file;
-    std::string instance = INSTANCES;
+    std::string instance = FALKENAUER_U;
 
     std::vector<int> weights;
 
-    file.open(instance+"/Falkenauer/Falkenauer_U/Falkenauer_u120_00.txt", std::ios::in);
+    file.open(instance+"/Falkenauer_u120_00.txt", std::ios::in);
     
     auto [num_itens, capacity] = readInstance(file,weights); // C++17
 
