42c0d8003617281baf32494054a33774b91e1aa3
nicolassoam <nsm4456@gmail.com>
2023-07-02 13:00:40 -0300

    corrigindo .bat

--- trabalho-4/execute.bat
+++ trabalho-4/execute.bat
@@ -1,10 +1,10 @@
 @echo off
 make -C .\build 
-
+setlocal EnableDelayedExpansion
 
 set num_runs=20
 
 for /l %%i in (1,1,%num_runs%) do (
-    start /wait .\build\main %%i
+    start /wait .\build\main !RANDOM!
 )
 
